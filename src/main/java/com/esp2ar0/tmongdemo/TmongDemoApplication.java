8102f82007eca6f9b24d26d836cfc71519afcf67
ChangHwan-Lee <esp2ar0@gmail.com>
2019-07-16 00:37:33 +0900

    first commit

---
+++ src/main/java/com/esp2ar0/tmongdemo/TmongDemoApplication.java
@@ -0,0 +1,13 @@
+package com.esp2ar0.tmongdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmongDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmongDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/esp2ar0/tmongdemo/TmongDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.esp2ar0.tmongdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TmongDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
